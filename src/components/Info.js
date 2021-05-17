1bc70464be795d401bc7571de552729042ccd3b5
AlbanRenahy <albanrenahy.pro@gmail.com>
2021-05-17 18:05:37 +0200

    Set Github context

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { GithubContext } from '../context/context';
 
 const UserInfo = () => {
-  return <h2>user info component</h2>;
+    const data = React.useContext(GithubContext)
+  return <h2>user info component : {data}</h2>;
 };
 
 export default UserInfo;
