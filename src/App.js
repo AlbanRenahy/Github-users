f344ad2fce00bee3419511713582e1271b481751
AlbanRenahy <albanrenahy.pro@gmail.com>
2021-05-17 17:40:35 +0200

    set router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import { Dashboard, Login, Error } from './pages';
+import React from "react";
+import { Dashboard, Login, Error } from "./pages";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Dashboard></Dashboard>
-      <Login />
-      <Error />
+        <Router>
+          <Switch>
+            <Route path="/" exact={true}>
+              <Dashboard></Dashboard>
+            </Route>
+            <Route path="/login">
+              <Login></Login>
+            </Route>
+            <Route path="*">
+              <Error></Error>
+            </Route>
+          </Switch>
+        </Router>
     </div>
   );
 }
