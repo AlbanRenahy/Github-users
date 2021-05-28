13b6fa9223b29dd53221572492c6dc2788af75af
AlbanRenahy <albanrenahy.pro@gmail.com>
2021-05-28 14:48:14 +0200

    Create and display the User and Followers components

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,6 @@ import Repos from "./Repos";
 import User from "./User";
 import Search from "./Search";
 import Navbar from "./Navbar";
+import Card from "./Card";
 
 export { Info, Repos, User, Search, Navbar };
