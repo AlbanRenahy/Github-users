1cac122de54585eedac0b8edc6be6975e8059d08
AlbanRenahy <albanrenahy.pro@gmail.com>
2021-05-29 22:07:53 +0200

    Style and display the different charts of repos

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -4,7 +4,6 @@ import { GithubContext } from '../context/context';
 import { Pie, Column, Bar, Doughnut } from './Charts';
 const Repos = () => {
   const { repos } = React.useContext(GithubContext);
-  console.log(repos);
 
   const languages = repos.reduce((total, item) => {
     const { language, stargazers_count } = item;
@@ -27,10 +26,42 @@ const Repos = () => {
     })
     .slice(0, 5);
 
+  // most stars per language
+
+  const mostPopular = Object.values(languages)
+    .sort((a, b) => {
+      return b.stars - a.stars;
+    })
+    .map((item) => {
+      return { ...item, value: item.stars };
+    })
+    .slice(0, 5);
+
+  // stars, forks
+
+  let { stars, forks } = repos.reduce(
+    (total, item) => {
+      const { stargazers_count, name, forks } = item;
+      total.stars[stargazers_count] = { label: name, value: stargazers_count };
+      total.forks[forks] = { label: name, value: forks };
+      return total;
+    },
+    {
+      stars: {},
+      forks: {},
+    }
+  );
+
+  stars = Object.values(stars).slice(-5).reverse();
+  forks = Object.values(forks).slice(-5).reverse();
+
   return (
     <section className='section'>
       <Wrapper className='section-center'>
         <Pie data={mostUsed} />
+        <Column data={stars} />
+        <Doughnut data={mostPopular} />
+        <Bar data={forks} />
       </Wrapper>
     </section>
   );
